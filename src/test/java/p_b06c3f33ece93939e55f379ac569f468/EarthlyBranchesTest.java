77598fbf39f8116e0accc54b21e6472d7865a59d
Jin Kwon <onacit@users.noreply.github.com>
2020-12-19 00:10:10 +0900

    wip

---
+++ src/test/java/p_b06c3f33ece93939e55f379ac569f468/EarthlyBranchesTest.java
@@ -0,0 +1,20 @@
+package p_b06c3f33ece93939e55f379ac569f468;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.util.regex.Pattern;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+class EarthlyBranchesTest { // 干支Test
+
+    @Test
+    void test_REGEXP() {
+        final Pattern pattern = Pattern.compile(地支.REGEXP);
+        for (final 地支 value : 地支.values()) {
+            assertThat(pattern.matcher(value.name()).matches()).isTrue();
+        }
+    }
+}
--- src/test/java/p_b06c3f33ece93939e55f379ac569f468/StemAndBranchTest.java
+++ src/test/java/p_b06c3f33ece93939e55f379ac569f468/StemAndBranchTest.java
@@ -4,11 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashSet;
+import java.util.regex.Pattern;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
-class 干支Test {
+class StemAndBranchTest { // 干支Test
 
     @Test
     void test_VALUES() {
@@ -20,10 +21,10 @@ class 干支Test {
 
     @Test
     void testOf() {
-        final 干支.天干[] 干们 = 干支.天干.values();
-        final 干支.地支[] 支们 = 干支.地支.values();
-        for (干支.天干 干 : 干们) {
-            for (干支.地支 支 : 支们) {
+        final 天干[] 干们 = 天干.values();
+        final 地支[] 支们 = 地支.values();
+        for (天干 干 : 干们) {
+            for (地支 支 : 支们) {
                 final 干支 value = 干支.of(干, 支);
                 assertThat(value).isNotNull().satisfies(v -> {
                     assertThat(干支.VALUES.indexOf(v)).isEqualTo(干支.index(干, 支));
