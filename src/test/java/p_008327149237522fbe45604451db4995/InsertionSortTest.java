987e4be883076df42cbeca51aabe5482a0d6e8af
Jin Kwon <onacit@users.noreply.github.com>
2021-10-10 13:18:27 +0900

    wip

--- src/test/java/p_008327149237522fbe45604451db4995/InsertionSortTest.java
+++ src/test/java/p_008327149237522fbe45604451db4995/InsertionSortTest.java
@@ -4,9 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
@@ -17,17 +17,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 @Slf4j
 class InsertionSortTest {
 
+    // ----------------------------------------------------------------------------------------------------------- sort1
     @Test
-    void sort1__Integers() {
-        final List<Integer> unsorted = IntStream.range(0, 16)
+    void sort1__Integers_ArrayList() {
+        final List<Integer> unsorted = IntStream.range(0, 128)
                 .map(i -> ThreadLocalRandom.current().nextInt()).boxed()
                 .collect(Collectors.toList());
-        log.debug("unsorted: {}", unsorted);
         final List<Integer> sorted1 = new ArrayList<>(unsorted);
         Collections.sort(sorted1);
         final List<Integer> sorted2 = new ArrayList<>(unsorted);
         InsertionSort.sort1(sorted2);
-        log.debug("sorted2: {}", sorted2);
         assertThat(sorted2)
                 .isSorted()
                 .isSortedAccordingTo(Comparator.naturalOrder())
@@ -35,16 +34,30 @@ class InsertionSortTest {
     }
 
     @Test
-    void sort2__Integers() {
-        final List<Integer> unsorted = IntStream.range(0, 16)
+    void sort1__Integers_LinkedList() {
+        final List<Integer> unsorted = IntStream.range(0, 128)
                 .map(i -> ThreadLocalRandom.current().nextInt()).boxed()
                 .collect(Collectors.toList());
-        log.debug("unsorted: {}", unsorted);
+        final List<Integer> sorted1 = new LinkedList<>(new ArrayList<>(unsorted));
+        Collections.sort(sorted1);
+        final List<Integer> sorted2 = new LinkedList<>(new ArrayList<>(unsorted));
+        InsertionSort.sort1(sorted2);
+        assertThat(sorted2)
+                .isSorted()
+                .isSortedAccordingTo(Comparator.naturalOrder())
+                .hasSameElementsAs(sorted1);
+    }
+
+    // ----------------------------------------------------------------------------------------------------------- sort2
+    @Test
+    void sort2__Integers_ArrayList() {
+        final List<Integer> unsorted = IntStream.range(0, 128)
+                .map(i -> ThreadLocalRandom.current().nextInt()).boxed()
+                .collect(Collectors.toList());
         final List<Integer> sorted1 = new ArrayList<>(unsorted);
         Collections.sort(sorted1);
         final List<Integer> sorted2 = new ArrayList<>(unsorted);
         InsertionSort.sort2(sorted2);
-        log.debug("sorted2: {}", sorted2);
         assertThat(sorted2)
                 .isSorted()
                 .isSortedAccordingTo(Comparator.naturalOrder())
@@ -52,11 +65,26 @@ class InsertionSortTest {
     }
 
     @Test
-    void sort3__Integers() {
+    void sort2__Integers_LinkedList() {
+        final List<Integer> unsorted = IntStream.range(0, 16)
+                .map(i -> ThreadLocalRandom.current().nextInt()).boxed()
+                .collect(Collectors.toList());
+        final List<Integer> sorted1 = new LinkedList<>(new ArrayList<>(unsorted));
+        Collections.sort(sorted1);
+        final List<Integer> sorted2 = new LinkedList<>(new ArrayList<>(unsorted));
+        InsertionSort.sort2(sorted2);
+        assertThat(sorted2)
+                .isSorted()
+                .isSortedAccordingTo(Comparator.naturalOrder())
+                .hasSameElementsAs(sorted1);
+    }
+
+    // ----------------------------------------------------------------------------------------------------------- sort3
+    @Test
+    void sort3__Integers_ArrayList() {
         final List<Integer> unsorted = IntStream.range(0, 128)
                 .map(i -> ThreadLocalRandom.current().nextInt()).boxed()
                 .collect(Collectors.toList());
-//        final List<Integer> unsorted = Arrays.asList(2, 0, 3, 1);
         final List<Integer> sorted1 = new ArrayList<>(unsorted);
         Collections.sort(sorted1);
         final List<Integer> sorted2 = new ArrayList<>(unsorted);
@@ -68,19 +96,48 @@ class InsertionSortTest {
     }
 
     @Test
-    void sort__ints() {
-        final List<Integer> unsorted = IntStream.range(0, 3)
-                .map(i -> ThreadLocalRandom.current().nextInt(0, 10)).boxed()
+    void sort3__Integers_LinkedList() {
+        final List<Integer> unsorted = IntStream.range(0, 128)
+                .map(i -> ThreadLocalRandom.current().nextInt()).boxed()
                 .collect(Collectors.toList());
-//        final List<Integer> unsorted = Arrays.asList(2, 0, 3, 1);
-        log.debug("unsorted: {}", unsorted);
+        final List<Integer> sorted1 = new LinkedList<>(new ArrayList<>(unsorted));
+        Collections.sort(sorted1);
+        final List<Integer> sorted2 = new LinkedList<>(new ArrayList<>(unsorted));
+        InsertionSort.sort3(sorted2);
+        assertThat(sorted2)
+                .isSorted()
+                .isSortedAccordingTo(Comparator.naturalOrder())
+                .hasSameElementsAs(sorted1);
+    }
+
+    // ----------------------------------------------------------------------------------------------------------- sort3
+    @Test
+    void sort4__Integers_ArrayList() {
+        final List<Integer> unsorted = IntStream.range(0, 128)
+                .map(i -> ThreadLocalRandom.current().nextInt()).boxed()
+                .collect(Collectors.toList());
         final List<Integer> sorted1 = new ArrayList<>(unsorted);
         Collections.sort(sorted1);
         final List<Integer> sorted2 = new ArrayList<>(unsorted);
-        InsertionSort.sort(sorted2);
-        log.debug("sorted2: {}", sorted2);
+        InsertionSort.sort4(sorted2);
         assertThat(sorted2)
                 .isSorted()
+                .isSortedAccordingTo(Comparator.naturalOrder())
+                .hasSameElementsAs(sorted1);
+    }
+
+    @Test
+    void sort4__Integers_LinkedList() {
+        final List<Integer> unsorted = IntStream.range(0, 128)
+                .map(i -> ThreadLocalRandom.current().nextInt()).boxed()
+                .collect(Collectors.toList());
+        final List<Integer> sorted1 = new LinkedList<>(new ArrayList<>(unsorted));
+        Collections.sort(sorted1);
+        final List<Integer> sorted2 = new LinkedList<>(new ArrayList<>(unsorted));
+        InsertionSort.sort4(sorted2);
+        assertThat(sorted2)
+                .isSorted()
+                .isSortedAccordingTo(Comparator.naturalOrder())
                 .hasSameElementsAs(sorted1);
     }
 }
