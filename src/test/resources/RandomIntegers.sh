cf639f70edd49106cd83f28e95dc55ca6f11f748
Jin Kwon <onacit@gmail.com>
2021-10-11 21:53:19 +0900

    Update RandomIntegers.sh

--- src/test/resources/RandomIntegers.sh
+++ src/test/resources/RandomIntegers.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-for count in 128 256 512 1024 2048 4096 8192 16384 32768; do
+for count in 128 256 512 1024 2048 4096 8192; do
     echo $count
-    for i in $(seq $count); do
-	od -vAn -N2 -td2 < /dev/urandom >> RandomIntegers$count.txt
+    #for i in $(seq $count); do
+	#od -vAn -N2 -td2 < /dev/urandom >> RandomIntegers$count.txt
 	#echo $SRANDOM >> RandomIntegers$count.txt
-    done
+	hexdump -n $((4*$count)) -e '/4 "%d\n"' /dev/urandom > RandomIntegers$count.txt
+    #done
 done
