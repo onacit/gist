c7f8d425bdcbf77eaa3e688181f675fd9c326bef
Jin Kwon <onacit@users.noreply.github.com>
2021-10-11 18:19:35 +0900

    wip

---
+++ src/test/java/p_008327149237522fbe45604451db4995/InsertionSortBenchmarkArrayList.java
@@ -0,0 +1,72 @@
+package p_008327149237522fbe45604451db4995;
+
+import lombok.extern.slf4j.Slf4j;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.TearDown;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@BenchmarkMode({Mode.SingleShotTime})
+@State(Scope.Benchmark)
+@Slf4j
+public class InsertionSortBenchmarkArrayList {
+
+    public static void main(final String... args) throws RunnerException {
+        final Options options = new OptionsBuilder()
+                .include(InsertionSortBenchmarkArrayList.class.getName() + ".*")
+                .build();
+        new Runner(options).run();
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+    @Benchmark
+    public void sort1() {
+        InsertionSort.sort1(new ArrayList<>(unsorted));
+    }
+
+    @Benchmark
+    public void sort2() {
+        InsertionSort.sort2(new ArrayList<>(unsorted));
+    }
+
+    @Benchmark
+    public void sort3() {
+        InsertionSort.sort3(new ArrayList<>(unsorted));
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+    @Setup
+    public void onSetup() {
+        log.debug("setting up...");
+        unsorted = Collections.unmodifiableList(
+                IntStream.range(0, 128)
+//                IntStream.range(0, 1048576)
+                        .map(i -> ThreadLocalRandom.current().nextInt())
+                        .boxed()
+                        .collect(Collectors.toList())
+        );
+    }
+
+    @TearDown
+    public void onTearDown() {
+        log.debug("tearing down...");
+        unsorted = null;
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+    private List<Integer> unsorted = null;
+}
--- src/test/java/p_0dd925d3768fcd4f79494ae32905dc1b/SelectionSortTest.java
+++ src/test/java/p_0dd925d3768fcd4f79494ae32905dc1b/SelectionSortTest.java
@@ -1,76 +1,124 @@
 package p_0dd925d3768fcd4f79494ae32905dc1b;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class SelectionSortTest {
 
-    @Test
-    void sort__Integers() {
-        final List<Integer> unsorted = IntStream.range(0, 16)
-                .map(i -> ThreadLocalRandom.current().nextInt()).boxed()
-                .collect(Collectors.toList());
+    private static Stream<List<Integer>> unsortedListsOfIntegers() {
+        return Stream.of(
+                IntStream.of(2, 0, 3, 1).mapToObj(Integer::new).collect(Collectors.toList()),
+                IntStream.of(2, 0, 0, 3, 1).mapToObj(Integer::new).collect(Collectors.toList()),
+                IntStream.of(3, 7, 4, 9, 5, 2, 6, 1).mapToObj(Integer::new).collect(Collectors.toList()),
+                IntStream.range(0, 128)
+                        .map(i -> ThreadLocalRandom.current().nextInt())
+                        .mapToObj(Integer::new)
+                        .collect(Collectors.toList())
+        );
+    }
+
+    // ----------------------------------------------------------------------------------------------------------- sort1
+    @MethodSource({"unsortedListsOfIntegers"})
+    @ParameterizedTest
+    void sort1__Integers_ArrayList(final List<Integer> unsorted) {
         final List<Integer> sorted1 = new ArrayList<>(unsorted);
         Collections.sort(sorted1);
         final List<Integer> sorted2 = new ArrayList<>(unsorted);
-        SelectionSort.sort(sorted2);
+        SelectionSort.sort1(sorted2);
         assertThat(sorted2)
                 .isSorted()
-                .hasSameElementsAs(sorted1);
+                .isSortedAccordingTo(Comparator.naturalOrder())
+                .usingElementComparator((o1, o2) -> o1 == o2 ? 0 : -1)
+                .containsExactlyElementsOf(sorted1) // nothing else in the same order
+        ;
     }
 
-    @Test
-    void sort__IntegersReverse() {
-        final List<Integer> unsorted = IntStream.range(0, 16)
-                .map(i -> ThreadLocalRandom.current().nextInt()).boxed()
-                .collect(Collectors.toList());
-        final Comparator<Integer> comparator = Comparator.reverseOrder();
+    @MethodSource({"unsortedListsOfIntegers"})
+    @ParameterizedTest
+    void sort1__Integers_LinkedList(final List<Integer> unsorted) {
+        final List<Integer> sorted1 = new LinkedList<>(new ArrayList<>(unsorted));
+        Collections.sort(sorted1);
+        final List<Integer> sorted2 = new LinkedList<>(new ArrayList<>(unsorted));
+        SelectionSort.sort1(sorted2);
+        assertThat(sorted2)
+                .isSorted()
+                .isSortedAccordingTo(Comparator.naturalOrder())
+                .usingElementComparator((o1, o2) -> o1 == o2 ? 0 : -1)
+                .containsExactlyElementsOf(sorted1)
+        ;
+    }
+
+    // ----------------------------------------------------------------------------------------------------------- sort2
+    @MethodSource({"unsortedListsOfIntegers"})
+    @ParameterizedTest
+    void sort2__Integers_ArrayList(final List<Integer> unsorted) {
         final List<Integer> sorted1 = new ArrayList<>(unsorted);
         Collections.sort(sorted1);
-        Collections.reverse(sorted1);
         final List<Integer> sorted2 = new ArrayList<>(unsorted);
-        SelectionSort.sort(sorted2, comparator);
-        assertThat(sorted2)
-                .isSortedAccordingTo(comparator)
-                .hasSameElementsAs(sorted1);
-    }
-
-    @Test
-    void sort__Longs() {
-        final List<Long> unsorted = IntStream.range(0, 16)
-                .mapToLong(i -> ThreadLocalRandom.current().nextLong()).boxed()
-                .collect(Collectors.toList());
-        final List<Long> sorted1 = new ArrayList<>(unsorted);
-        Collections.sort(sorted1);
-        final List<Long> sorted2 = new ArrayList<>(unsorted);
-        SelectionSort.sort(sorted2);
+        SelectionSort.sort2(sorted2);
         assertThat(sorted2)
                 .isSorted()
-                .hasSameElementsAs(sorted1);
+                .isSortedAccordingTo(Comparator.naturalOrder())
+                .usingElementComparator((o1, o2) -> o1 == o2 ? 0 : -1)
+                .containsExactlyElementsOf(sorted1) // nothing else in the same order
+        ;
     }
 
-    @Test
-    void sort__LongsReverse() {
-        final List<Long> unsorted = IntStream.range(0, 16)
-                .mapToLong(i -> ThreadLocalRandom.current().nextLong()).boxed()
-                .collect(Collectors.toList());
-        final Comparator<Long> comparator = Comparator.reverseOrder();
-        final List<Long> sorted1 = new ArrayList<>(unsorted);
+    @MethodSource({"unsortedListsOfIntegers"})
+    @ParameterizedTest
+    void sort2__Integers_LinkedList(final List<Integer> unsorted) {
+        final List<Integer> sorted1 = new LinkedList<>(new ArrayList<>(unsorted));
         Collections.sort(sorted1);
-        Collections.reverse(sorted1);
-        final List<Long> sorted2 = new ArrayList<>(unsorted);
-        SelectionSort.sort(sorted2, comparator);
+        final List<Integer> sorted2 = new LinkedList<>(new ArrayList<>(unsorted));
+        SelectionSort.sort2(sorted2);
         assertThat(sorted2)
-                .isSortedAccordingTo(comparator)
-                .hasSameElementsAs(sorted1);
+                .isSorted()
+                .isSortedAccordingTo(Comparator.naturalOrder())
+                .usingElementComparator((o1, o2) -> o1 == o2 ? 0 : -1)
+                .containsExactlyElementsOf(sorted1)
+        ;
+    }
+
+    // ----------------------------------------------------------------------------------------------------------- sort3
+    @MethodSource({"unsortedListsOfIntegers"})
+    @ParameterizedTest
+    void sort3__Integers_ArrayList(final List<Integer> unsorted) {
+        final List<Integer> sorted1 = new ArrayList<>(unsorted);
+        Collections.sort(sorted1);
+        final List<Integer> sorted2 = new ArrayList<>(unsorted);
+        SelectionSort.sort3(sorted2);
+        assertThat(sorted2)
+                .isSorted()
+                .isSortedAccordingTo(Comparator.naturalOrder())
+                .usingElementComparator((o1, o2) -> o1 == o2 ? 0 : -1)
+                .containsExactlyElementsOf(sorted1) // nothing else in the same order
+        ;
+    }
+
+    @MethodSource({"unsortedListsOfIntegers"})
+    @ParameterizedTest
+    void sort3__Integers_LinkedList(final List<Integer> unsorted) {
+        final List<Integer> sorted1 = new LinkedList<>(new ArrayList<>(unsorted));
+        Collections.sort(sorted1);
+        final List<Integer> sorted2 = new LinkedList<>(new ArrayList<>(unsorted));
+        SelectionSort.sort3(sorted2);
+        assertThat(sorted2)
+                .isSorted()
+                .isSortedAccordingTo(Comparator.naturalOrder())
+                .usingElementComparator((o1, o2) -> o1 == o2 ? 0 : -1)
+                .containsExactlyElementsOf(sorted1)
+        ;
     }
 }
