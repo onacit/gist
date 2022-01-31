c0e556a548d535cef3c3acd43d1648b1db32bf19
Jin Kwon <onacit@gmail.com>
2022-01-31 15:01:26 +0900

    wip

---
+++ src/test/java/g_79ff0bed607bef315c4353b374e1319e/TowerOfHanoiTest.java
@@ -0,0 +1,21 @@
+package g_79ff0bed607bef315c4353b374e1319e;
+
+import org.junit.jupiter.api.Test;
+
+class TowerOfHanoiTest {
+
+    @Test
+    void main__1() {
+        TowerOfHanoi.main("1");
+    }
+
+    @Test
+    void main__2() {
+        TowerOfHanoi.main("2");
+    }
+
+    @Test
+    void main__4() {
+        TowerOfHanoi.main("4");
+    }
+}
